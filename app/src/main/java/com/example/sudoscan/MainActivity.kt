3c2ce28ccc22a55395fdd548c578ef7cf729ff79
Amlan101 <amlan.sahooricky@gmail.com>
2024-07-10 19:50:11 +0530

    Added the necessary dependencies, including the openCV SDK, Retrofit, and RoomDB

--- app/src/main/java/com/example/sudoscan/MainActivity.kt
+++ app/src/main/java/com/example/sudoscan/MainActivity.kt
@@ -1,20 +1,21 @@
 package com.example.sudoscan
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import org.opencv.android.OpenCVLoader
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        if (OpenCVLoader.initDebug()) {
+            Log.d("OpenCV", "OpenCV successfully loaded!")
+            Toast.makeText(this, "OpenCV successfully loaded!", Toast.LENGTH_SHORT).show()
+        } else {
+            Log.d("OpenCV", "OpenCV failed to load!")
+            Toast.makeText(this, "OpenCV not loaded!", Toast.LENGTH_SHORT).show()
         }
     }
 }
